02a92f3b16758d33ba27124a7497989b5c22be0a
Kevin N <63832406+kevin-nyquist@users.noreply.github.com>
2023-10-09 20:51:10 -0400

    First phase comments

---
+++ simulator/close.py
@@ -0,0 +1,8 @@
+import os
+import time
+
+# Execute the shutdown command (requires root or sudo permissions)
+time.sleep(10)
+# os.system("sudo shutdown -h now")
+
+# We want to simulate a shutdown like above
--- simulator/main.py
+++ simulator/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 from EnergyLoader import EnergyLoader 
 from Timer import Timer
 from Battery import Battery
-
+# import DataCollector from DataCollector
 
 timer = Timer(init_time="2015-07-10")
 battery = Battery( soc=4000, capacity=5000, base_consume=5)
@@ -21,4 +21,20 @@ def Simulator():
 
 
     return None
+
+
+# battery monitoring function - Kevin
+# def battery_monitor() :
+    # monitor the power stage
+
+    # set a frequency during the sensing operation interval (every 10 sec)
+    # test different frequencies
+
+    # trigger the power failure event at certain battery % level
+
+
+# definition of the power draw for each sensor - Kevin
+
+# def power_failure(int soc) :
+    # battery.drain(soc, soc) use this function to drop the soc to simulate battery dying
 
