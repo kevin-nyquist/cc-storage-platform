0997e6b7391dbae848829e0d209b75aa029e8fc2
Kevin N <63832406+kevin-nyquist@users.noreply.github.com>
2023-10-09 21:38:19 -0400

    Added read.py for reading sensor values

--- simulator/close.py
+++ simulator/close.py
@@ -2,7 +2,8 @@ import os
 import time
 
 # Execute the shutdown command (requires root or sudo permissions)
-time.sleep(10)
+# time.sleep(10)
+print("Hello")
 # os.system("sudo shutdown -h now")
 
 # We want to simulate a shutdown like above
